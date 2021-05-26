69b2de9578a8a2336db9342c90bde65347c77667
Harshal Kolhe <harshalkolhe0@gmail.com>
2021-05-26 12:09:11 +0530

    First git Commit

---
+++ src/main/java/com/qa/Pages/LoginPage.java
@@ -0,0 +1,10 @@
+package com.qa.Pages;
+
+public class LoginPage {
+
+	public void start()
+	{
+		System.out.println("Login.Pages.Start");
+	}
+	
+}
---
+++ src/test/java/com/qa/Tests/LoginTest.java
@@ -0,0 +1,10 @@
+package com.qa.Tests;
+
+public class LoginTest {
+
+	public void start()
+	{
+		System.out.println("Login.Test.Start");
+	}
+	
+}
